e6c818e93483f77b6a1962ae7a5925f0d84f0892
Kenny Code <kenny030217@gmail.com>
2018-07-06 21:56:09 -0300

    Init work

---
+++ README.md
@@ -0,0 +1,26 @@
+# BETA Project, not already to "use"
+# 1. Shoumen web application
+
+Shoumen web application is Java EE Web application that use a Gaikujin business transaction dependent project.
+
+## 1.1 Gaikujin business transaction.
+
+Gaikujin business transaction that project only have business classes and it will provided simple actions like create account, client, transaction and more.
+
+* [Shoumen web application](https://github.com/eduardobento2/shoumen) - clone it
+* [Gaikujin business transaction](https://github.com/eduardobento2/gaikujin) - clone it
+
+## How to use
+
+Import all projects your eclipse and use maven install in gaikujin to generate and install gaikujin in your maven repository.
+
+## Versioning
+ BETA Project, not already to "use"
+ 
+## Authors
+
+* **Eduardo Bento** - *Initial work* - [Eduardobrj](https://github.com/eduardobento2)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ src/main/java/br/com/kennycode/webstore/Web.java
@@ -0,0 +1,47 @@
+package br.com.kennycode.webstore;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.kennycode.gaikujin.model.Account;
+
+@WebServlet(urlPatterns="/timeline")
+public class Web extends HttpServlet {
+
+	/**
+	* 
+	*/
+	private static final long serialVersionUID = -7549846059463730204L;
+	private String message;
+
+	public void init() throws ServletException {
+		// Do required initialization
+		
+		Account a = new Account();
+		a.setBank("bank X");
+		
+		message = "Hello World - "+ a.getBank();
+
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+	}
+
+	public void destroy() {
+		// do nothing.
+	}
+
+}
